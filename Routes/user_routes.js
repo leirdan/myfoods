233ea7c54633d0bfb79fa44b0b20a1af480d9357
leirdan <uouleirdan@gmail.com>
2022-10-08 21:22:37 -0300

    teste de rotas

--- Routes/user_routes.js
+++ Routes/user_routes.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 const UserCtrl = require("../controller/UserCtrl");
 
-router.get("/login", UserCtrl.login);
-router.post("/login", UserCtrl.enter);
+router.get("/user/login", UserCtrl.login);
+router.post("/user/login", UserCtrl.enter);
 router.get("/logout", UserCtrl.logout);
 router.get("/register", UserCtrl.register);
 router.post("/register/success", UserCtrl.create);
